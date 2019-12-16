6dbd532f4c81aa7940dce41ae04395111c215614
Jordan Samhi <j.samhi@me.com>
2019-12-16 11:12:33 +0100

    Add: command line options

--- src/main/java/com/github/JordanSamhi/doesApkContainParticularLocalType/Main.java
+++ src/main/java/com/github/JordanSamhi/doesApkContainParticularLocalType/Main.java
@@ -3,6 +3,8 @@ package com.github.JordanSamhi.doesApkContainParticularLocalType;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.github.JordanSamhi.doesApkContainParticularLocalType.Utils.CommandLineOptions;
+
 import soot.Local;
 import soot.PackManager;
 import soot.Scene;
@@ -13,10 +15,14 @@ import soot.options.Options;
 public class Main {
 
 	public static void main(String[] args) {
+
+		CommandLineOptions options = new CommandLineOptions(args);
+
 		List<String> apk = new ArrayList<String>();
-		apk.add(args[0]);
-		
-		String androidJarPAth = "";
+		apk.add(options.getApk());
+
+		String androidJarPAth = options.getPlatforms();
+		String type = options.getType();
 
 		soot.G.reset();
 		Options.v().set_allow_phantom_refs(true);
@@ -31,13 +37,15 @@ public class Main {
 			for(SootMethod sm : sc.getMethods()) {
 				if(sm.hasActiveBody()) {
 					for (Local l : sm.getActiveBody().getLocals()) {
-						if(l.getType().toString().equals("android.app.AlarmManager")) {
+						if(l.getType().toString().equals(type)) {
+							System.out.println("[+] Apk contains " + type);
 							System.exit(1);
 						}
 					}
 				}
 			}
 		}
+		System.out.println("[!] Apk does not contain " + type);
 		System.exit(0);
 	}
 }
---
+++ src/main/java/com/github/JordanSamhi/doesApkContainParticularLocalType/Utils/CommandLineOptions.java
@@ -0,0 +1,118 @@
+package com.github.JordanSamhi.doesApkContainParticularLocalType.Utils;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.javatuples.Triplet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This class sets the different option for the application
+ * @author Jordan Samhi
+ *
+ */
+public class CommandLineOptions {
+
+	private static final Triplet<String, String, String> APK = new Triplet<String, String, String>("apk", "a", "Apk file");
+	private static final Triplet<String, String, String> HELP = new Triplet<String, String, String>("help", "h", "Print this message");
+	private static final Triplet<String, String, String> TYPE = new Triplet<String, String, String>("type", "t", "Type to search for in Apk");
+	private static final Triplet<String, String, String> PLATFORMS =
+			new Triplet<String, String, String>("platforms", "p", "Android platforms folder");
+	private static final String doesApkContainParticularLocalType = "doesApkContainParticularLocalType";
+	
+	private Options options, firstOptions;
+	private CommandLineParser parser;
+	private CommandLine cmdLine, cmdFirstLine;
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	public CommandLineOptions(String[] args) {
+		this.options = new Options();
+		this.firstOptions = new Options();
+		this.initOptions();
+		this.parser = new DefaultParser();
+		this.parse(args);
+	}
+
+	/**
+	 * This method does the parsing of the arguments.
+	 * It distinguished, real options and help option.
+	 * @param args the arguments of the application
+	 */
+	private void parse(String[] args) {
+		HelpFormatter formatter = null;
+		try {
+			this.cmdFirstLine = this.parser.parse(this.firstOptions, args, true);
+			if (this.cmdFirstLine.hasOption(HELP.getValue0())) {
+				formatter = new HelpFormatter();
+				formatter.printHelp(doesApkContainParticularLocalType, this.options, true);
+				System.exit(0);
+			}
+			this.cmdLine = this.parser.parse(this.options, args);
+		} catch (ParseException e) {
+			this.logger.error(e.getMessage());
+			System.exit(1);
+		}
+	}
+
+	/**
+	 * Initialization of all recognized options
+	 */
+	private void initOptions() {
+		final Option file = Option.builder(APK.getValue1())
+				.longOpt(APK.getValue0())
+				.desc(APK.getValue2())
+				.hasArg(true)
+				.argName(APK.getValue0())
+				.required(true)
+				.build();
+
+		final Option platforms = Option.builder(PLATFORMS.getValue1())
+				.longOpt(PLATFORMS.getValue0())
+				.desc(PLATFORMS.getValue2())
+				.hasArg(true)
+				.argName(PLATFORMS.getValue0())
+				.required(true)
+				.build();
+		
+		final Option type = Option.builder(TYPE.getValue1())
+				.longOpt(TYPE.getValue0())
+				.desc(PLATFORMS.getValue2())
+				.hasArg(true)
+				.argName(TYPE.getValue0())
+				.required(true)
+				.build();
+
+		final Option help = Option.builder(HELP.getValue1())
+				.longOpt(HELP.getValue0())
+				.desc(HELP.getValue2())
+				.argName(HELP.getValue0())
+				.build();
+
+		this.firstOptions.addOption(help);
+
+		this.options.addOption(file);
+		this.options.addOption(platforms);
+		this.options.addOption(type);
+		for(Option o : this.firstOptions.getOptions()) {
+			this.options.addOption(o);
+		}
+	}
+
+	public String getApk() {
+		return this.cmdLine.getOptionValue(APK.getValue0());
+	}
+
+	public String getPlatforms() {
+		return this.cmdLine.getOptionValue(PLATFORMS.getValue0());
+	}
+	
+	public String getType() {
+		return this.cmdLine.getOptionValue(TYPE.getValue0());
+	}
+}
