1c605558fb007430527aeac491c0bf67a8744293
Jordan Samhi <j.samhi@me.com>
2020-01-29 15:42:42 +0100

    Update: use scene transformer

--- src/main/java/com/github/JordanSamhi/doesApkContainParticularLocalType/Main.java
+++ src/main/java/com/github/JordanSamhi/doesApkContainParticularLocalType/Main.java
@@ -10,11 +10,13 @@ import org.slf4j.LoggerFactory;
 import com.github.JordanSamhi.doesApkContainParticularLocalType.Utils.CommandLineOptions;
 
 import soot.Body;
-import soot.BodyTransformer;
 import soot.G;
 import soot.Local;
 import soot.PackManager;
 import soot.Scene;
+import soot.SceneTransformer;
+import soot.SootClass;
+import soot.SootMethod;
 import soot.Transform;
 import soot.options.Options;
 
@@ -33,18 +35,29 @@ public class Main {
 
 		initializeSoot(platforms, apk);
 
-		PackManager.v().getPack("jtp").add(
-				new Transform("jtp.myTransform", new BodyTransformer() {
-					protected void internalTransform(Body body, String phase, @SuppressWarnings("rawtypes") Map options) {
-						for (Local l : body.getLocals()) {
-							if(l.getType().toString().equals(type)) {
-								logger.info(String.format("Apk contains %s", type));
-								logger.info(String.format("-- Method: %s", body.getMethod()));
-								System.exit(1);
+		PackManager.v().getPack("wjtp").add(
+				new Transform("wjtp.myTransform", new SceneTransformer() {
+					protected void internalTransform(String phaseName,
+							@SuppressWarnings("rawtypes") Map options) {
+						for(SootClass sc : Scene.v().getApplicationClasses()) {
+							if(!isSystemClass(sc.getName()) && sc.isConcrete()) {
+								for(SootMethod sm : sc.getMethods()) {
+									if(sm.isConcrete()) {
+										Body b = sm.retrieveActiveBody();
+										for (Local l : b.getLocals()) {
+											if(l.getType().toString().equals(type)) {
+												logger.info(String.format("Apk contains %s", type));
+												logger.info(String.format("-- Method: %s", b.getMethod()));
+												System.exit(1);
+											}
+										}
+									}
+								}
 							}
 						}
 					}
 				}));
+
 		PackManager.v().runPacks();
 		logger.warn("Apk does not contain " + type);
 		System.exit(0);
@@ -53,12 +66,19 @@ public class Main {
 	private static void initializeSoot(String platforms, String apk) {
 		G.reset();
 		Options.v().set_src_prec(Options.src_prec_apk);
-		Options.v().set_output_dir("/dev/null");
 		Options.v().set_allow_phantom_refs(true);
 		Options.v().set_android_jars(platforms);
+		Options.v().set_whole_program(true);
 		List<String> apps = new ArrayList<String>();
 		apps.add(apk);
 		Options.v().set_process_dir(apps);
 		Scene.v().loadNecessaryClasses();
+	}
+
+	private static boolean isSystemClass(String className) {
+		return (className.startsWith("android.") || className.startsWith("java.") || className.startsWith("javax.")
+				|| className.startsWith("sun.") || className.startsWith("org.omg.")
+				|| className.startsWith("org.w3c.dom.") || className.startsWith("com.google.")
+				|| className.startsWith("com.android."));
 	}
 }
